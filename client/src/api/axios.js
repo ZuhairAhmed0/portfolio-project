11a9acd77e1ebfd84b6699aa63fa52db5c0740b4
Zuhair Ahmed <78954298+ZuhairAhmed0@users.noreply.github.com>
2024-03-25 00:23:41 +0200

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3500";
-// const baseURL = "https://portfolio-api-fry1.onrender.com";
+// const baseURL = "http://localhost:3500";
+const baseURL = "https://portfolio-api-fry1.onrender.com";
 export default axios.create({
   baseURL,
 });
